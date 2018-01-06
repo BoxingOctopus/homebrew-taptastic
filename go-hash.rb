0e2bcc6e2a346807005a21223f97d7b3c8a66c34
Ryan Draga <ryan.draga@scotiabank.com>
2018-01-06 12:27:46 -0500

    fix sha256 hash for go-hash formula

--- go-hash.rb
+++ go-hash.rb
@@ -2,7 +2,7 @@ class Go-hash < Formula
   desc "Small utility to store secret information like passwords"
   homepage "https://github.com/renatoathaydes/go-hash/"
   url "https://github.com/renatoathaydes/go-hash/releases/download/0.6.0/go-hash-darwin-amd64"
-  sha256 "cb67c08ece570cdac599d87ad295e46b1a195213ecf18ad986b584e671cbf2c2"
+  sha256 "9bb416438d8c6556df65001146b53970632459801e7fdea029e88ddff64506f7"
 
   def install
     bin.install "go-hash-darwin-amd64"
